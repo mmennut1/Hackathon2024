e69480bcd350eb4f1d93b81935759d4bb8082940
Conner Broglio <connerbroglio@Conners-MacBook-Air.local>
2024-02-17 21:08:57 -0500

    shaddows

--- script.js
+++ script.js
@@ -51,3 +51,13 @@ function calcRoute() {
     });
 
 }
+
+var options = {
+    types: ['(cities)']
+}
+
+var input1 = document.getElementById("from");
+var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+
+var input2 = document.getElementById("to");
+var autocomplete2 = new google.maps.places.Autocomplete(input2, options);

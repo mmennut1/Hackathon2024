f629289365548c2ee54b8398085aae538e4818a4
Matteo Mennuti <mmennut1@binghamton.edu>
2024-02-18 08:37:30 -0500

    javascript update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function initMap() {
 
     const directionsRenderer = new google.maps.DirectionsRenderer();
 
-    const map = new google.maps.Map(document.getElementById("map"), {
+    const map = new google.maps.Map(document.getElementById("googleMap"), {
         zoom: 7,
         center: { lat: 41.85, lng: -87.65 } // Default center (Chicago)
     });
@@ -12,25 +12,24 @@ function initMap() {
     const startInput = document.getElementById("start");
     const endInput = document.getElementById("end");
 
-
     const startAutocomplete = new google.maps.places.Autocomplete(startInput);
     startAutocomplete.bindTo("bounds", map);
 
     const endAutocomplete = new google.maps.places.Autocomplete(endInput);
     endAutocomplete.bindTo("bounds", map);
 
+    const placesService = new google.maps.PlacesService(map);
+
     const onChangeHandler = function () {
-        calculateAndDisplayRoute(directionsService, directionsRenderer);
+        calculateAndDisplayRoute(directionsService, directionsRenderer, placesService);
     };
     document.getElementById("show-route").addEventListener("click", onChangeHandler);
 }
 
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-    const placesService = new google.maps.placesService();
-    
+function calculateAndDisplayRoute(directionsService, directionsRenderer, placesService) {
     const start = document.getElementById("start").value;
     const end = document.getElementById("end").value;
-    const placeType = document.getElementById("type").value;
+    let placeType = document.getElementById("LocationType").value;
     const locationRadius = document.getElementById("radius").value;
 
     switch(placeType) {
@@ -45,35 +44,42 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
             break;
     }
 
-    // Set the intermediate waypoint to Pittsburgh
-    var placeOfInterest = {
+    console.log("Start:", start);
+    console.log("End:", end);
+    console.log("Place Type:", placeType);
+    console.log("Location Radius:", locationRadius);
+
+    var request = {
         location: new google.maps.LatLng(42.0987, -75.9154),
         radius: locationRadius,
         type: [placeType],
-        rankby: prominence
+        rankby: google.maps.places.RankBy.PROMINENCE
     };
 
-    directionsService.route(
-        {
-            origin: start,
-            destination: end,
-            waypoints: [{ location: placesService.nearbySearch(placeOfInterest, callbackPOI), stopover: true }],
-            optimizeWaypoints: true,
-            travelMode: google.maps.TravelMode.DRIVING,
-        },
-        (response, status) => {
-            if (status === "OK") {
-                directionsRenderer.setDirections(response);
-            } else {
-                window.alert("Directions request failed due to " + status);
-            }
+    let poi;
+    placesService.nearbySearch(request, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            poi = results[0];
+            directionsService.route(
+                {
+                    origin: start,
+                    destination: end,
+                    waypoints: [{ location: poi.geometry.location, stopover: true }],
+                    optimizeWaypoints: true,
+                    travelMode: google.maps.TravelMode.DRIVING,
+                },
+                (response, status) => {
+                    if (status === "OK") {
+                        directionsRenderer.setDirections(response);
+                    } else {
+                        window.alert("Directions request failed due to " + status);
+                    }
+                }
+            );
+        } else {
+            window.alert("Places service failed due to " + status);
         }
-    );
-}
-
-function callbackPOI(results, status) {
-    if(status === placesService.OK)
-        return results[0];
+    });
 }
 
 window.onload = initMap;

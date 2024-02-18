92bd960277c767ee29cbfb545350322c9b5e41a0
Matteo Mennuti <mmennut1@binghamton.edu>
2024-02-17 22:48:30 -0500

    places implementation

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function initMap() {
     const directionsService = new google.maps.DirectionsService();
+
     const directionsRenderer = new google.maps.DirectionsRenderer();
-    const map = new google.maps.Map(document.getElementById('googleMap'), {
+
+    const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 7,
         center: { lat: 41.85, lng: -87.65 } // Default center (Chicago)
     });
@@ -9,6 +11,7 @@ function initMap() {
 
     const startInput = document.getElementById("start");
     const endInput = document.getElementById("end");
+
 
     const startAutocomplete = new google.maps.places.Autocomplete(startInput);
     startAutocomplete.bindTo("bounds", map);
@@ -23,17 +26,38 @@ function initMap() {
 }
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+    const placesService = new google.maps.placesService();
+    
     const start = document.getElementById("start").value;
     const end = document.getElementById("end").value;
+    const placeType = document.getElementById("type").value;
+    const locationRadius = document.getElementById("radius").value;
+
+    switch(placeType) {
+        case "Bar":
+            placeType = "bar";
+            break;
+        case "Casino":
+            placeType = "casino";
+            break;
+        case "Night Club":
+            placeType = "night_club";
+            break;
+    }
 
     // Set the intermediate waypoint to Pittsburgh
-    const pittsburgh = "Pittsburgh, Pennsylvania";
+    var placeOfInterest = {
+        location: new google.maps.LatLng(42.0987, -75.9154),
+        radius: locationRadius,
+        type: [placeType],
+        rankby: prominence
+    };
 
     directionsService.route(
         {
             origin: start,
             destination: end,
-            waypoints: [{ location: pittsburgh, stopover: true }],
+            waypoints: [{ location: placesService.nearbySearch(placeOfInterest, callbackPOI), stopover: true }],
             optimizeWaypoints: true,
             travelMode: google.maps.TravelMode.DRIVING,
         },
@@ -45,6 +69,11 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
             }
         }
     );
+}
+
+function callbackPOI(results, status) {
+    if(status === placesService.OK)
+        return results[0];
 }
 
 window.onload = initMap;
